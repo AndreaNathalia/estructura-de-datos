4294a21c8fcf93b177554819c51df61ec58947e9
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-01-19 23:14:31 -0600

    add Tarea 2

---
+++ Tarea2/rocket_api/src/test/java/com/rocket/rocket_api/Tarea2Test.java
@@ -0,0 +1,13 @@
+package com.rocket.rocket_api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Tarea2Test {
+
+    @Test
+    void index() {
+        assertEquals(3279287, 3279287);
+    }
+}
