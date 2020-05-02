325f32d06bb75b693bf7ccc584afe2ca9dfa0ffc
AndreaNathalia <49224603+AndreaNathalia@users.noreply.github.com>
2020-05-02 02:59:42 -0600

    add insertion sort

---
+++ Tarea14/insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(array):
+    for i in range (1, len(array)):
+        k = array[i]
+
+        move = i - 1
+        while move >= 0 and k < array[move]:
+            array[move + 1] = array[move]
+            move -= 1
+        
+        array[move + 1] = k
+
+
+array = [5, 4, 3, 2, 1]
+insertion_sort(array)
+
+#PRINT
+for printt in range(len(array)):
+    print(array[printt])
